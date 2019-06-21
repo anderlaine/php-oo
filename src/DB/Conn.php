f47e4585e8035003d09e3779dabb0beac1131eea
Anderson de Carvalho Ferreira <anderlaine@live.com>
2019-06-21 10:00:31 -0400

    Update Conn.php

--- src/DB/Conn.php
+++ src/DB/Conn.php
@@ -11,7 +11,7 @@ class Conn{
 
     public static function getInstancia(){
         if(is_null(self::$instancia)){
-            self::$instancia = new \PDO('mysql:host=localhost;dbname=formacao_php','anderson','budega');
+            self::$instancia = new \PDO('mysql:host=localhost;dbname=formacao_php','','');
         }
 
         return self::$instancia;
